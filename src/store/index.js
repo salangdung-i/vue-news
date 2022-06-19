8f709ed5d4417502d1c1ac48a732e2e88d769c26
salangdung-i <dladmswl92@naver.com>
2022-06-19 15:38:51 +0900

    자바스크립트 비동기 처리(1) - Callback

---
+++ src/store/index.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { fetchNewsList, fetchAskList, fetchJobsList } from "../api/index.js";
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    news: [],
+    jobs: [],
+    asks: [],
+  },
+
+  mutations: {
+    // 3. mutations에서  state에 넣어줌
+    SET_NEWS(state, data) {
+      state.news = data;
+    },
+    SET_JOBS(state, data) {
+      state.jobs = data;
+    },
+    SET_ASKS(state, data) {
+      state.asks = data;
+    },
+  },
+  // 비동기 호출 하는 것
+  actions: {
+    FETCH_NEWS(context) {
+      // 1. api 호출
+      fetchNewsList()
+        .then((response) => {
+          // 2. mutations 에 넣어줌
+          context.commit("SET_NEWS", response.data); // api 이용해서 muations에 data 넣어줌.
+        })
+        .catch((error) => console.log(error));
+    },
+    FETCH_JOBS(context) {
+      fetchJobsList()
+        .then((response) => {
+          context.commit("SET_JOBS", response.data);
+        })
+        .catch((error) => console.log(error));
+    },
+    FETCH_ASKS(context) {
+      fetchAskList()
+        .then((response) => {
+          context.commit("SET_ASKS", response.data);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+});
